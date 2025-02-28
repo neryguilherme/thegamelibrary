f5a11da39a5aa8d273eff4d3724f6bff7d060469
Guilherme Rocha <nrocha.guilherme@gmail.com>
2025-02-28 19:49:22 -0300

    Arquivos para a Previsao feita a partir do input do usuário no streamlit

---
+++ pages/classif_st.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import streamlit as st
+import re 
+import prediction as pred
+import matplotlib.pyplot as plt
+from collections import Counter
+
+# Carregar o dataset Parquet
+df = pd.read_parquet("..\games.parquet")
+
+# Obter valores únicos da coluna "Supported languages"
+supported_languages = []
+invalid_chars = re.compile(r"[;/\\_&\[\]]")  
+for languages in df["Supported languages"].dropna():
+    languages = languages.strip("[]")  
+    found_languages = re.findall(r"'([^']+)'", languages)  # Encontrar valores entre aspas simples
+    for lang in found_languages:
+        if not invalid_chars.search(lang) and lang not in supported_languages:
+            supported_languages.append(lang)
+
+# Obter valores únicos da coluna "Categories"
+unique_categories = []
+for categories in df['Categories'].dropna():
+    list_categories = categories.split(",")
+    for category in list_categories:
+        if category not in unique_categories:
+            unique_categories.append(category)
+
+# Obter valores únicos da coluna "Categories"
+unique_tags = []
+for tags in df['Tags'].dropna():
+    list_tags = tags.split(",")
+    for tag in list_tags:
+        if tag not in unique_tags:
+            unique_tags.append(tag)
+
+# Obter faixa de preços da coluna "Price"
+min_price = df["Price"].min()
+max_price = df["Price"].max()
+st.title("Selecione a faixa de preço em Dolares")
+price_range = st.slider("Faixa de Preço", float(min_price), float(max_price), (float(min_price), float(max_price)))
+
+# Criar a interface com Streamlit
+st.title("Selecione uma Tag")
+
+# Criar um dropdown
+selected_tag = st.selectbox("Tags", unique_tags)
+
+st.write(f"Você selecionou: {selected_tag}")
+
+# Criar a interface com Streamlit
+st.title("Selecione uma Categoria")
+
+# Criar um dropdown
+selected_category = st.selectbox("Categorias", unique_categories)
+
+st.write(f"Você selecionou: {selected_category}")
+
+# Criar a interface com Streamlit
+st.title("Selecione um idioma suportado")
+
+# Criar um dropdown
+selected_language = st.selectbox("Idioma suportado", supported_languages)
+
+st.write(f"Você selecionou: {selected_language}")
+
+if st.button("Aplicar Filtros"):
+    # Lista de categorias a serem contadas
+    categorias = ['Indie', 'Action', 'Casual', 'Outros']
+
+    # Lista de previsões (substituir por valores reais)
+    previsao = pred.prediction(price_range[0], price_range[1], selected_tag, selected_category, selected_language)
+
+    # Contar as ocorrências de cada categoria
+    contagem = Counter(previsao)
+
+    total = sum(contagem.values())
+
+    # Garantir que todas as categorias tenham um valor na contagem e calcular porcentagem
+    contagem_final = {categoria: (contagem.get(categoria, 0) / total) * 100 for categoria in categorias}
+
+    # Criar o gráfico de barras com eixos invertidos
+    fig, ax = plt.subplots()
+    ax.barh(list(contagem_final.keys()), list(contagem_final.values()), color=['blue', 'red', 'green', 'gray'])
+    ax.set_ylabel('Categorias')
+    ax.set_xlabel('Porcentagem (%)')
+    ax.set_title('Frequência de Categorias na Previsão (Porcentagem)')
+
+    # Exibir gráfico no Streamlit
+    st.pyplot(fig)
---
+++ pages/prediction.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+
+def filter_data(data, min_price, max_price, tag, category, language):
+    data = data[
+        (data["Price"] >= min_price) &
+        (data["Price"] <= max_price) &
+        (data["Tags"].str.contains(tag)) &
+        (data["Categories"].str.contains(category)) &
+        (data["Supported languages"].str.contains(language))
+    ]
+
+    print(data.shape)
+    return data
+
+def process_data(dataset):
+    databank = dataset.copy()
+
+    label_encoders = {}
+    x_column = databank.copy()
+    for column in x_column.select_dtypes(include=['object']).columns:
+        le = LabelEncoder()
+        x_column[column] = le.fit_transform(x_column[column])
+        label_encoders[column] = le
+    
+    k = x_column['Genres']
+    databank = x_column.drop(columns= ['Genres'])
+
+    scaler = StandardScaler()
+    numerical_cols = x_column.select_dtypes(include=['number']).columns
+    databank[numerical_cols] = scaler.fit_transform(x_column[numerical_cols])
+
+    le = LabelEncoder()
+    k = le.fit_transform(k)
+
+    return x_column, k, label_encoders
+
+def run_rf(X, y, label_encoders):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    rf_model = RandomForestClassifier(n_estimators=100, max_depth=30, min_samples_split=10, min_samples_leaf=4, max_features='sqrt', random_state=42)
+    
+    rf_model.fit(X_train, y_train)
+
+    prediction_encoded = rf_model.predict(X_test)
+    
+    prediction = label_encoders['Genres'].inverse_transform(prediction_encoded)
+
+    return prediction
+    
+
+def prediction(min_price, max_price, tag, category, language):
+    database = pd.read_parquet('..\games_preprocessed.parquet')
+
+    data_filtered = filter_data(database, min_price, max_price, tag, category, language)
+    X, y, label_enc = process_data(data_filtered)
+
+    predicted = run_rf(X, y, label_enc)
+
+    return predicted
